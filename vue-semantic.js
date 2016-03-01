ae9d4fa84aed5f4dd9b84192f97b0d6026275a87
Brock Reece <brockreece@gmail.com>
2016-03-01 16:07:28 +0000

    Use semantic ui transitions

--- vue-semantic.js
+++ vue-semantic.js
@@ -18,5 +18,80 @@ module.exports = {
         Vue.component('semantic-popup', Popup);
         Vue.component('semantic-radio', Radiobutton);
         Vue.component('semantic-statistic', Statistic);
+
+        var emphasis = [
+            'flash',
+            'shake',
+            'pulse',
+            'tada',
+            'bounce',
+            'jiggle',
+        ]
+
+        var appearance = [
+            'slide up',
+            'slide down',
+            'vertical flip',
+            'horizontal flip',
+            'fade',
+            'fade up',
+            'fade down',
+            'scale',
+            'drop',
+            'browse',
+        ]
+
+        emphasis.forEach(function (animation) {
+            var definition = {
+                enter: function (el, done) {
+                    $(el)
+                        .transition('reset')
+                        .transition(animation + ' in', 700, done)
+
+                    return function() {
+                        $(el).transition('stop')
+                    }
+                },
+
+                leave: function (el, done) {
+                    $(el)
+                        .transition('reset')
+                        .transition(animation + ' out', 700, done)
+
+                    return function() {
+                        $(el).transition('stop')
+                    }
+                }
+            }
+
+            Vue.transition(animation, definition)
+        })
+
+        appearance.forEach(function (animation) {
+            var definition = {
+                enter: function (el, done) {
+                    $(el)
+                        .transition('reset')
+                        .transition(animation + ' in', 700, done)
+
+                    return function() {
+                        $(el).transition('stop')
+                    }
+                },
+
+                leave: function (el, done) {
+                    $(el)
+                        .transition('reset')
+                        .transition(animation + ' out', 700, done)
+
+                    return function() {
+                        $(el).transition('stop')
+                    }
+                }
+            }
+            var id = animation.split(' ').join('-')
+
+            Vue.transition(id, definition)
+        })
     },
 }
