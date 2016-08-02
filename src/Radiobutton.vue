b82535f9f104cbfe754be32ea31fb756f76a30af
BrockReece <brockreece@gmail.com>
2016-08-02 12:20:14 +0100

    Redo radio vue component

--- src/Radiobutton.vue
+++ src/Radiobutton.vue
@@ -1,41 +1,26 @@
-
 <template>
-<span>
-  <div class="ui form">
-    <div class="grouped fields">
-      <div class="field" v-for="option in data">
-        <div class="ui radio checkbox">
-          <input type="radio" name="{{field_name}}" class="native" @click.prevent="checkOption(option.value)">
-          <label>{{{option.label}}}</label>
+    <div class="ui form">
+        <div class="grouped fields">
+            <div class="field" v-for="option in data">
+                <div class="ui radio checkbox">
+                    <input v-model="model" type="radio" :value="option.value">
+                    <label>{{{option.label}}}</label>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
-</span>
-
 </template>
 
 <script>
-
-export default {
-
-      props: {
-        element_id: {},
-        field_name: {},
-        field_value: {},
-        data: {}
-    },
-
-    methods: {
-        checkOption(value) {
-            this.field_value = value;
-        }
-    },
-    computed: {
-        isChecked(value) {
-            return this.field_value == value;
-        }
+    export default {
+        props: {
+            model: {
+                required: true,
+                twoWay: true,
+            },
+            data: {
+                required: true,
+            },
+        },
     }
-}
-
 </script>
